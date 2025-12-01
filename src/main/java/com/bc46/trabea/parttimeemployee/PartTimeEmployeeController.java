1a02d240d57df2412bbf329b711f0322dc921565
Alexander <romian37@gmaiil.com>
2025-12-01 09:51:38 +0700

    simplify endpoints API

--- src/main/java/com/bc46/trabea/parttimeemployee/PartTimeEmployeeController.java
+++ src/main/java/com/bc46/trabea/parttimeemployee/PartTimeEmployeeController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("employees/part-timers")
+@RequestMapping("part-timers")
 @RequiredArgsConstructor
 public class PartTimeEmployeeController {
     private final WorkScheduleService workScheduleService;
